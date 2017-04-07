0436489606e2dc00f2d57d30fba0a520e8cccdbd
saintedlama <christoph.walcher@gmail.com>
2017-04-07 10:46:36 +0200

    Fix release script

--- _release.js
+++ _release.js
@@ -1,30 +1,29 @@
 var shell = require('shelljs');
 
-if (exec('git status --porcelain').output) {
-  console.error('Git working directory not clean. Please commit all chances to release a new package to npm.');
+if (exec('git status --porcelain').stdout) {
+  console.error('Git working directory not clean.');
   process.exit(2);
 }
 
-var versionIncrement = process.argv[process.argv.length - 1];
-var versionIncrements = ['major', 'minor', 'patch'];
+var versionIncrement = process.argv[process.argv.length -1];
 
-if (versionIncrements.indexOf(versionIncrement) < 0) {
+if (versionIncrement != 'major' && versionIncrement != 'minor' && versionIncrement != 'patch') {
   console.error('Usage: node release.js major|minor|patch');
-  process.exit(1)
+  process.exit(1);
 }
 
-exec('npm test');
 exec('npm version ' + versionIncrement);
 
+exec('npm test');
 exec('git push');
 exec('git push --tags');
 exec('npm publish');
 
-function exec (cmd) {
-  var ret = shell.exec(cmd, { silent: true });
+function exec(cmd) {
+  var ret = shell.exec(cmd, { silent : true });
 
-  if (ret.code !== 0) {
-    console.error(ret.output);
+  if (ret.code != 0) {
+    console.error(ret.stderr);
     process.exit(1);
   }
 
